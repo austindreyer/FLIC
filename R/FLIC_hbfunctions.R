d508ce4cf43bc6ab9a6fbcc83addc7ee41172871
Austin Dreyer <31745574+austindreyer@users.noreply.github.com>
2018-04-09 10:26:55 -0500

    Update FLIC_hbfunctions.R

--- R/FLIC_hbfunctions.R
+++ R/FLIC_hbfunctions.R
@@ -1,4 +1,4 @@
-## Updated 3/27/18 ##
+## Updated 4/9/18 ##
 
 ######## FLIC HomeBrew Functions ########
 
@@ -507,92 +507,6 @@ color.select <- function(gencol)
   return(color)
 }
 
-
-## this is stuff for making plots with peak feeding time highlighted with vertical bars
-ggplot(data = data.pdfdbtl, aes(x=hour, y=mean, color=genotype)) + geom_line() +
-  geom_ribbon(data=data.pdfdbtl,aes(ymin=mean-se,ymax=mean+se, fill = genotype), alpha=0.25, colour=NA) +
-  scale_color_manual(values = c("iso>DBT(L)"="black", "pdf>DBT(L)" = "red", "pdf>iso" = "gray")) +
-  scale_fill_manual(values = c("iso>DBT(L)"="black", "pdf>DBT(L)" = "red", "pdf>iso" = "gray"))+
-  theme_classic() +
-  theme(axis.line.x = element_line(color="black", size = .5),
-        axis.line.y = element_line(color="black", size = .5),
-        axis.title = element_text(size=18),
-        plot.title = element_text(hjust = 0.5),
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))
-
-ggplot(data = data.pdfdbtl.only, aes(x=hour, y=mean, color="red")) + geom_line() +
-  geom_ribbon(data=data.pdfdbtl.only,aes(ymin=mean-se,ymax=mean+se, fill = "red"), alpha=0.25, colour=NA) +
-  theme_classic() +
-  theme(axis.line.x = element_line(color="black", size = .5),
-        axis.line.y = element_line(color="black", size = .5),
-        axis.title = element_text(size=18),
-        plot.title = element_text(hjust = 0.5),
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12)) +
-        geom_segment(aes(x = data.pdfdbtl.only[3,1], y = 0.25, 
-                         xend = data.pdfdbtl.only[3,1], yend = 0),
-                     colour="red", size=0.5, linetype = "longdash") +
-        geom_segment(aes(x = data.pdfdbtl.only[70,1], y = 0.25, 
-                   xend = data.pdfdbtl.only[70,1], yend = 0),
-                   colour="red", size=0.5, linetype = "longdash")+
-        geom_segment(aes(x = data.pdfdbtl.only[125,1], y = 0.25, 
-                   xend = data.pdfdbtl.only[125,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash")+
-        geom_segment(aes(x = data.pdfdbtl.only[168,1], y = 0.25, 
-                   xend = data.pdfdbtl.only[168,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash")+
-        geom_segment(aes(x = data.pdfdbtl.only[229,1], y = 0.25, 
-                   xend = data.pdfdbtl.only[229,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash") +
-  geom_segment(aes(x = data.dbtiso.only[3,1], y = 0.2, 
-                   xend = data.dbtiso.only[3,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash") +
-  geom_segment(aes(x = data.dbtiso.only[66,1], y = 0.2, 
-                   xend = data.dbtiso.only[66,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[112,1], y = 0.2, 
-                   xend = data.dbtiso.only[112,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[157,1], y = 0.2, 
-                   xend = data.dbtiso.only[157,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[212,1], y = 0.2, 
-                   xend = data.dbtiso.only[212,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")
-
-
-+
-  geom_segment(aes(x = data.pdfdbtl.only[3,1], y = data.pdfdbtl.only[3,2], 
-                   xend = data.pdfdbtl.only[3,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash") +
-  geom_segment(aes(x = data.pdfdbtl.only[70,1], y = data.pdfdbtl.only[70,2], 
-                   xend = data.pdfdbtl.only[70,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.pdfdbtl.only[125,1], y = data.pdfdbtl.only[125,2], 
-                   xend = data.pdfdbtl.only[125,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.pdfdbtl.only[168,1], y = data.pdfdbtl.only[168,2], 
-                   xend = data.pdfdbtl.only[168,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.pdfdbtl.only[229,1], y = data.pdfdbtl.only[229,2], 
-                   xend = data.pdfdbtl.only[229,1], yend = 0),
-               colour="red", size=0.5, linetype = "longdash") +
-  geom_segment(aes(x = data.dbtiso.only[3,1], y = data.dbtiso.only[3,2], 
-                   xend = data.dbtiso.only[3,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash") +
-  geom_segment(aes(x = data.dbtiso.only[66,1], y = data.dbtiso.only[66,2], 
-                   xend = data.dbtiso.only[66,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[112,1], y = data.dbtiso.only[112,2], 
-                   xend = data.dbtiso.only[112,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[157,1], y = data.dbtiso.only[157,2], 
-                   xend = data.dbtiso.only[157,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")+
-  geom_segment(aes(x = data.dbtiso.only[212,1], y = data.dbtiso.only[212,2], 
-                   xend = data.dbtiso.only[212,1], yend = 0),
-               colour="black", size=0.5, linetype = "longdash")
 
 ## Function to find local maxima and minima 
 
